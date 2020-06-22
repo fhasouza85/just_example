14f04ccc3fc6b44c2cbb6af1ee3aec567885bd9f
fabio <hemerson.souza1985@gmail.com>
2020-06-22 18:12:31 -0300

    Testando commit usando Rstudio

---
+++ histogram.R
@@ -0,0 +1,7 @@
+x <- mtcars$mpg
+h<-hist(x, breaks=10, col="red", xlab="Miles Per Gallon",
+   main="Histogram with Normal Curve")
+xfit<-seq(min(x),max(x),length=40)
+yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
+yfit <- yfit*diff(h$mids[1:2])*length(x)
+lines(xfit, yfit, col="blue", lwd=2)
